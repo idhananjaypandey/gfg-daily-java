7da2403f64f3325ff6f218919c542d53e191e7fd
Dhananjay <dhananjay.pandey72@gmail.com>
2026-05-23 23:56:04 +0530

    Transform to Sum Tree

--- may-2026/Transform to Sum Tree.java
+++ may-2026/Transform to Sum Tree.java
@@ -1,1 +1,44 @@
 // Transform to Sum Tree
+
+/* Structure for Tree Node
+class Node {
+    int data;
+    Node left, right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+};
+*/
+class Solution {
+
+    // Function to convert tree into Sum Tree
+    public void toSumTree(Node root) {
+        solve(root);
+    }
+
+    // Returns sum of original subtree values
+    private int solve(Node node) {
+
+        // Base case
+        if (node == null) {
+            return 0;
+        }
+
+        // Store old value
+        int oldValue = node.data;
+
+        // Recursively get left and right subtree sums
+        int leftSum = solve(node.left);
+        int rightSum = solve(node.right);
+
+        // Update current node value
+        node.data = leftSum + rightSum;
+
+        // Return total sum including original node value
+        return node.data + oldValue;
+    }
+}
