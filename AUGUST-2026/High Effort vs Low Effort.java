2b718971e0860ef246cec58f5a4830a10b4e2f17
Dhananjay <dhananjay.pandey72@gmail.com>
2026-08-10 12:57:36 +0530

    High Effort vs Low Effort

---
+++ AUGUST-2026/High Effort vs Low Effort.java
@@ -0,0 +1,19 @@
+// High Effort vs Low Effort
+
+class Solution {
+    public int maxTask(int[] h, int[] l) {
+        int n = h.length;
+        if (n == 0) return 0;
+        if (n == 1) return Math.max(h[0], l[0]);
+
+        int[] dp = new int[n];
+        dp[0] = Math.max(h[0], l[0]);
+        dp[1] = Math.max(h[1], Math.max(dp[0] + l[1], l[0] + l[1]));
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + l[i], dp[i - 2] + h[i]);
+        }
+
+        return dp[n - 1];
+    }
+}
