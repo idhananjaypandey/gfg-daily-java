38986a58f2bfaa71f189af4f2ef91bc9669e8d62
Dhananjay <dhananjay.pandey72@gmail.com>
2026-08-19 11:55:00 +0530

    #19: Triplets with Sum in Range

---
+++ AUGUST-2026/Triplets with Sum in Range.java
@@ -0,0 +1,32 @@
+// Triplets with Sum in Range
+
+class Solution {
+    public int countTriplets(int[] arr, int l, int r) {
+        Arrays.sort(arr);
+        return countTripletsLessThanOrEqual(arr, r) - countTripletsLessThanOrEqual(arr, l - 1);
+    }
+
+    private int countTripletsLessThanOrEqual(int[] arr, int target) {
+        int count = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (sum <= target) {
+                    // All pairs between left and right form a sum <= target with arr[i]
+                    count += (right - left);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return count;
+    }
+}
